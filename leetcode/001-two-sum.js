5b147cc7fed13d250eb424aa2cb831800a2783fd
Tsogtbayar <mtsogtbayar24@gmail.com>
2026-06-07 21:53:30 +0900

    day01: node.js basics + fs module + two sum
    hello.js and files.js written and verified working. Fixed empty
    package.json (regenerated via npm init) and completed the Two Sum
    solution with a Map-based O(n) approach.

---
+++ leetcode/001-two-sum.js
@@ -0,0 +1,24 @@
+/**
+ * LeetCode #1 - Two Sum (Easy)
+ * Date: 2026-06-07 (Day 1)
+ * 
+ * Given an array of integers nums and an integer target,
+ * return indices of the two numbers such that they add up to target.
+ */
+
+var twoSum = function(nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
+        }
+
+        map.set(nums[i], i);
+    }
+};
+
+// Test
+console.log(twoSum([2,7,11,15], 9)); // [0,1]
