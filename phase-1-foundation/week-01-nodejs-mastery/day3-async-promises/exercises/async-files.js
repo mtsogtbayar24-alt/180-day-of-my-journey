d2fec6fa324258dd3b138eb214a36ba012206615
Tsogtbayar <mtsogtbayar24@gmail.com>
2026-08-29 21:20:41 +0700

    doing da3 till leetcode

--- phase-1-foundation/week-01-nodejs-mastery/day3-async-promises/exercises/async-files.js
+++ phase-1-foundation/week-01-nodejs-mastery/day3-async-promises/exercises/async-files.js
@@ -1,30 +1,54 @@
-// function wait(ms) {
-//   return new Promise(resolve => {
-//     setTimeout(resolve, ms);
-//   });
-// }
+ function wait(ms) {
+   return new Promise(resolve => {
+     setTimeout(resolve, ms);
+   });
+ }
 
-// async function run(){
-//   console.log("Start!");
+ async function run(){
+   console.log("Start!");
 
-//   await wait(1000);
+   await wait(1000);
 
-//   console.log("After 1 second");
+   console.log("After 1 second");
 
-//   console.log("End");
-// }
+   console.log("End");
+ }
 
-// run();
+ run();
+
+//⭐ fs.promises
 
 const fs = require("fs/promises");
 
 async function main() {
+  //Task 1
   await fs.writeFile("notes.txt", "I am learning Node.js async operations.\n");
+  //Task 3
   await fs.appendFile("notes.txt", "Today I learned fs.promises.\n");
 
+  //Task 2
   const data = await fs.readFile("notes.txt", "utf-8");
   console.log("File content:");
   console.log(data);
 }
 
 main();
+
+async function readFile() {
+  try {
+    await fs.writeFile("notes.txt", "I am learning Node.js async operations.\n");
+  //Task 3
+  await fs.appendFile("notes.txt", "Today I learned fs.promises.\n");
+
+  //Task 2
+  const data = await fs.readFile("does-not-exist.txt", "utf-8");
+  console.log("File content:");
+  console.log(data);
+
+  } catch(err){
+    console.log("Cound not read a file.")
+  }
+  
+};
+
+readFile();
